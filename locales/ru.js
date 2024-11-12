7e4ba6eef163525b2c5955b3970d6ba599815809
msedachev <msedachev@mts.ru>
2024-11-12 03:38:30 +0300

    added output of feeds and posts

--- locales/ru.js
+++ locales/ru.js
@@ -3,7 +3,11 @@ export default {
     errors: {
       invalidURL: 'Ссылка должна быть валидным URL',
       alreadyExist: 'RSS уже существует',
+      notContainRSS: 'Ресурс не содержит валидный RSS',
       networkError: 'Ошибка сети',
+      unknown: 'Неизвестная ошибка. Попробуйте еще раз',
+      serverError: 'Ошибка сервера: {{status}}',
+
     },
 
     success: 'RSS успешно загружен',
--- src/view.js
+++ src/view.js
@@ -1,7 +1,17 @@
 import { object, string, setLocale } from 'yup';
 import onChange from 'on-change';
 import i18next from 'i18next';
+import axios from 'axios';
 import resources from '../locales/index.js';
+
+const parseDOM = (data) => {
+  const parser = new DOMParser();
+  return parser.parseFromString(data, 'application/xml');
+};
+
+const routes = {
+  allOrigins: () => 'https://allorigins.hexlet.app/get?disableCache=true&url=',
+};
 
 export default () => {
   const state = {
@@ -9,7 +19,15 @@ export default () => {
       status: true,
       feedback: '',
     },
+    addedUrls: [],
+
+    modalWindow: {
+      active: false,
+      dataId: '',
+    },
+
     feeds: [],
+    posts: [],
   };
 
   const i18nextInstance = i18next.createInstance();
@@ -29,7 +47,7 @@ export default () => {
       url: string()
         .url(i18nextInstance.t('errors.invalidURL'))
         .required()
-        .notOneOf(state.feeds, i18nextInstance.t('errors.invalidURL')),
+        .notOneOf(state.addedUrls, i18nextInstance.t('errors.invalidURL')),
     });
     return schema.validate({ url });
   };
@@ -37,13 +55,58 @@ export default () => {
   const input = document.querySelector('#url-input');
   const form = document.querySelector('.rss-form');
   const feedBackEl = document.querySelector('.feedback');
+  const feedsDiv = document.querySelector('.feeds');
+  const postsDiv = document.querySelector('.posts');
+  const modalTitle = document.querySelector('.modal-title');
+  const modalBody = document.querySelector('.modal-body');
+  const modalLinkButton = document.querySelector('a.full-article');
 
   const watchedState = onChange(state, (path, currentValue) => {
     if (path === 'form.feedback') {
       feedBackEl.textContent = currentValue;
+      console.log('feedBackEl.textContent:', feedBackEl.textContent);
+      if (watchedState.form.status) {
+        feedBackEl.classList.remove('text-danger');
+        feedBackEl.classList.add('text-success');
+      } else {
+        feedBackEl.classList.remove('text-success');
+        feedBackEl.classList.add('text-danger');
+      }
     }
     if (path === 'form.status') {
       input.classList.toggle('is-invalid', !currentValue);
+    }
+    if (path === 'feeds') {
+      feedsDiv.innerHTML = `<div class="card border-0">
+      <div class="card-body"><h2 class="card-title h4">Фиды</h2></div>
+      <ul class="list-group border-0 rounded-0"></ul></div>`;
+      const list = feedsDiv.querySelector('ul');
+      state.feeds.forEach((feed) => {
+        const li = document.createElement('li');
+        li.classList.add('list-group-item', 'border-0', 'border-end-0');
+        li.innerHTML = `<h3 class="h6 m-0">${feed.title}</h3>
+        <p class="m-0 small text-black-50">${feed.description}</p>`;
+        list.appendChild(li);
+      });
+    }
+    if (path === 'posts') {
+      postsDiv.innerHTML = `<div class="card border-0">
+      <div class="card-body"><h2 class="card-title h4">Посты</h2></div>
+      <ul class="list-group border-0 rounded-0"></ul></div></div>`;
+      const list = postsDiv.querySelector('ul');
+      state.posts.forEach((post) => {
+        const li = document.createElement('li');
+        li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+        li.innerHTML = `<a href="${post.link}" class="fw-bold" data-id="${post.id}" target="_blank" rel="noopener noreferrer">${post.title}</a>
+        <button type="button" class="btn btn-outline-primary btn-sm" data-id="${post.id}" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button></li>`;
+        list.appendChild(li);
+      });
+    }
+    if (path === 'modalWindow.active') {
+      const currentPost = state.posts.find((post) => post.id === state.modalWindow.dataId);
+      modalTitle.textContent = currentPost.title;
+      modalBody.textContent = currentPost.description;
+      modalLinkButton.setAttribute('href', currentPost.link);
     }
   });
   form.addEventListener('submit', (e) => {
@@ -51,14 +114,57 @@ export default () => {
     const formData = new FormData(e.target);
     const url = formData.get('url');
     validateUrl(url)
-      .then(() => {
+      .then(async (validUrl) => {
+        watchedState.form.feedback = '';
         watchedState.form.status = true;
         watchedState.form.feedback = i18nextInstance.t('success');
-        watchedState.feeds.push(url);
+        watchedState.addedUrls.push(url);
+        console.log('validUrl:', validUrl);
+        return axios.get(`${routes.allOrigins()}${validUrl.url}`);
+      })
+      .then((response) => {
+        const result = parseDOM(response.data.contents);
+        console.log('result:', result);
+        if (result.querySelector('parsererror')) {
+          throw new Error(i18nextInstance.t('errors.notContainRSS'));
+        } else {
+          watchedState.addedUrls.push(url);
+          watchedState.form.feedback = i18nextInstance.t('success');
+        }
+        const feedtitle = result.querySelector('title');
+        const feedDescription = result.querySelector('description');
+        watchedState.feeds.push({
+          title: feedtitle.textContent,
+          description: feedDescription.textContent,
+        });
+        let idCounter = 0;
+
+        const uniqueId = () => {
+          idCounter += 1;
+          return idCounter;
+        };
+        const posts = result.querySelectorAll('item');
+        posts.forEach((post) => {
+          const postTitle = post.querySelector('title');
+          const postDescription = post.querySelector('description');
+          const postLink = post.querySelector('link');
+          watchedState.posts.push({
+            id: uniqueId(),
+            title: postTitle.textContent,
+            description: postDescription.textContent,
+            link: postLink.textContent,
+          });
+        });
       })
       .catch((error) => {
-        watchedState.form.feedback = error.message;
         watchedState.form.status = false;
+        if (axios.isAxiosError(error)) {
+          if (error.request) {
+            watchedState.form.feedback = i18nextInstance.t('errors.networkError');
+          }
+        } else {
+          watchedState.form.feedback = error.message;
+        }
       });
   });
 };
